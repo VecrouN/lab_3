d1d86f76948ecd7ca4807c73be47f74f760513e2
Vecrou_n <vnechaev00@gmail.com>
2020-11-18 00:23:50 +0300

    add GUI v1

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
+
 }
+
+// ЗАМЕТКИ
+// QFileDialog для файлового менеджера
+// OpenSSL для работы с ключами
+//
+
+// TODO
