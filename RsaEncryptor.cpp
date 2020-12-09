9dcd0d8bd4f37eea8f3ba2f73a822bec5ad4e96e
AlinaOpekunova <60748406+AlinaOpekunova@users.noreply.github.com>
2020-12-09 23:08:37 +0300

    Update RsaEncryptor.cpp

--- RsaEncryptor.cpp
+++ RsaEncryptor.cpp
@@ -9,25 +9,39 @@
 
 RsaEncryptor::RsaEncryptor()
 {
+    CryptoPP::Integer n("0xbeaadb3d839f3b5f"), e("0x11"), d("0x21a5ae37b9959db9");
+    m_PrivateRsaKey.Initialize(n,e,d);
+    m_PublicRsaKey.Initialize(e,d);
+}
+
+RsaEncryptor::RsaEncryptor(RsaEncryptor &other_):m_PrivateRsaKey(other_.m_PrivateRsaKey),
+    m_PublicRsaKey(other_.m_PublicRsaKey){
 
 }
 
-RsaEncryptor::RsaEncryptor(RsaEncryptor &other_)
+RsaEncryptor &RsaEncryptor::operator=(const RsaEncryptor& other_)
 {
-
-}
-
-RsaEncryptor &RsaEncryptor::operator=(const RsaEncryptor &other_)
-{
+    CryptoPP::Integer n("0xbeaadb3d839f3b5f"), e("0x11"), d("0x21a5ae37b9959db9");
+    m_PrivateRsaKey.Initialize(n,e,d);
+    m_PublicRsaKey.Initialize(e,d);
     if (this==&other_)
         return *this;
+    else
+        if ((other_.m_PrivateRsaKey.GetPrime1() == m_PrivateRsaKey.GetPrime1()||
+                other_.m_PrivateRsaKey.GetPrime2() == m_PrivateRsaKey.GetPrime2()||
+                other_.m_PrivateRsaKey.GetPrivateExponent() == m_PrivateRsaKey.GetPrivateExponent())||
+                (other_.m_PublicRsaKey.GetPublicExponent()==m_PublicRsaKey.GetPublicExponent()||
+                 other_.m_PublicRsaKey.GetModulus() == m_PublicRsaKey.GetModulus()))
+            return *this;
 
 
 }
 
 RsaEncryptor::~RsaEncryptor()
 {
-
+    CryptoPP::Integer n("0xbeaadb3d839f3b5f"), e("0x11"), d("0x21a5ae37b9959db9");
+    m_PrivateRsaKey.Initialize(n,e,d);
+    m_PublicRsaKey.Initialize(e,d);
 }
 
 void RsaEncryptor::rsaGenerateKey()
