d627f8aa68a3bec09015f159a65302b2143161ce
AlinaOpekunova <60748406+AlinaOpekunova@users.noreply.github.com>
2020-12-12 20:23:04 +0300

    Update RsaDecryptor.h

--- RsaDecryptor.h
+++ RsaDecryptor.h
@@ -12,14 +12,14 @@ public:
     RsaDecryptor(); // конструктор
     ~RsaDecryptor(); // деструктор
 
-    void readPrivateKey(const std::string& filename); // чтение файла с закрытым ключом RSA
-    std::string readKeyInFileForDecryptor(const std::string& filename); // чтение публичного ключа вместе с шифрованным ключом для AES
-    std::string rsaDecryptKey(std::string& aesKeyStringForDescryptor); // расшифровка ключа AES, получение зашифрованного ключа в виде строки
+    // расшифровка ключа AES, получение зашифрованного ключа в виде строки
+    std::string rsaDecryptKey(const std::string& aesKeyStringForDescryptor);
 
 protected:
 
 private:
 
 };
+
 
 #endif // RSADECRYPTOR_H
