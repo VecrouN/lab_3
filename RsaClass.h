fc427babdf331c9f64e3bec04766df4244995093
AlinaOpekunova <60748406+AlinaOpekunova@users.noreply.github.com>
2020-12-12 20:16:42 +0300

    Update RsaClass.h

--- RsaClass.h
+++ RsaClass.h
@@ -13,39 +13,43 @@ class RsaClass
 public:
     RsaClass();// конструктор
     RsaClass(RsaClass& other_); // конструктор копирования
-    RsaClass& operator=(const RsaClass& other_);// перегрузка оператора копирования
+    RsaClass& operator=(RsaClass& other_);// перегрузка оператора копирования
     ~RsaClass(); // деструктор
 
     void rsaGenerateKey(); // генерация ключей
-    CryptoPP::RSA::PublicKey getPublicRsaKey() const; // получение публичного ключа
-    CryptoPP::RSA::PrivateKey getPrivateRsaKey() const; // получение закрытого ключа
+    CryptoPP::RSA::PublicKey getPublicRsaKey() const; // получение значения поля открытого ключа
+    CryptoPP::RSA::PrivateKey getPrivateRsaKey() const; // получение значения из поля закрытого ключа
 
     // получение публичного ключа в виде строки
-    std::string getStringPublicRsaKey(CryptoPP::RSA::PublicKey m_PublicRsaKey) const;
-
+    std::string getStringPublicRsaKey(CryptoPP::RSA::PublicKey& m_PublicRsaKey) const;
     // получение закрытого ключа в виде строки
-    std::string getStringRsaPrivateKey(CryptoPP::RSA::PublicKey m_PrivateRsaKey) const;
+    std::string getStringRsaPrivateKey(CryptoPP::RSA::PublicKey& m_PrivateRsaKey) const;
 
-    // функция, которая устанавливает поле открытого ключа
-    void setPublicRsaKey(std::string publicRsaKeyString, CryptoPP::RSA::PublicKey m_PublicRsaKey);
+    // функция, которая устанавливает поле открытого ключа из строки
+    void setPublicRsaKey(std::string publicRsaKeyString);
+    // функция, которая устанавливает поле закрытого ключа из строки
+    void setPrivateRsaKey(std::string privateRsaKeyString);
 
-    // функция, которая устанавливает поле закрытого ключа
-    void setPrivateRsaKey(std::string privateRsaKeyString,CryptoPP::RSA::PublicKey m_PrivateRsaKey);
-    
-    void savePublicKey(const std::string& filename, CryptoPP::RSA::PublicKey m_PublicRsaKey);
-    void savePrivateKey(const std::string& filename, CryptoPP::RSA::PrivateKey m_PrivateRsaKey);
+    // функция, сохраняющая открытый ключ в специальный файл
+    void savePublicKey(const std::string& filename);
+    // функция, сохраняющая закрытый ключ в специальный файл
+    void savePrivateKey(const std::string& filename);
 
-    void LoadPublicKey(const std::string& filename,CryptoPP::RSA::PublicKey& m_PublicRsaKey);
-    void LoadPrivateKey(const std::string& filename,CryptoPP::RSA::PrivateKey& m_PrivateRsaKey);
-
-
+    // функция, загружающая открытый ключ из специального файл
+    void LoadPublicKey(const std::string& filename);
+    // функция, загружающая открытый ключ из специального файл
+    void LoadPrivateKey(const std::string& filename);
 
   protected:
-      CryptoPP::RSA::PublicKey m_PublicRsaKey; // открытый ключ шифрования
-      CryptoPP::RSA::PrivateKey m_PrivateRsaKey; // закрытый ключ шифрования
-    
+
+    CryptoPP::RSA::PublicKey m_PublicRsaKey; // открытый ключ шифрования
+    CryptoPP::RSA::PrivateKey m_PrivateRsaKey; // закрытый ключ шифрования
+
   private:
-    
+
 };
+
+#endif // RSACLASS_H
+
 
 #endif // RSACLASS_H
